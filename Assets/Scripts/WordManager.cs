a7a4ffe55ebbc68ac07f6f0541bf797ad3bbad09
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-05-16 13:37:00 +0700

    + Add function check for active word

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -5,6 +5,9 @@ public class WordManager : MonoBehaviour
 {
     List<Word> words = new List<Word>();
 
+    private bool hasActiveWord = false;
+    private Word activeWord;
+
     void Start()
     {
         WordGenerator.LoadWords();
@@ -16,15 +19,69 @@ public class WordManager : MonoBehaviour
         Word word = new Word(WordGenerator.GetRandomWord());
         words.Add(word);
     }
+
+    public void TypeLetter(char letter)
+    {
+        if (hasActiveWord)
+        {
+            // Check if letter was next
+            if (activeWord.GetNextLetter() == letter)
+            {
+                activeWord.TypeLetter();
+            }
+        }
+        else
+        {
+            foreach (Word word in words)
+            {
+                if (word.GetNextLetter() == letter)
+                {
+                    activeWord = word;
+                    hasActiveWord = true;
+                    word.TypeLetter();
+                    break;
+                }
+            }
+        }
+
+        if (hasActiveWord && activeWord.WordTyped())
+        {
+            hasActiveWord = false;
+            words.Remove(activeWord);
+        }
+    }
 }
 
 [System.Serializable]
 public class Word
 {
     public string word;
+    private int typeIndex;
     
     public Word(string _word)
     {
         word = _word;
+        typeIndex = 0;
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+        // Remove the letter on screen
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = typeIndex >= word.Length;
+        if (wordTyped)
+        {
+            // Remove the word on screen
+        }
+        return wordTyped;
     }
 }
