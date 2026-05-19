decff0da69147934d707bf872058591fe56b9a73
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-05-20 01:20:47 +0700

    * Update input check

--- Assets/Scripts/WordInput.cs
+++ Assets/Scripts/WordInput.cs
@@ -8,8 +8,21 @@ public class WordInput : MonoBehaviour
 
         foreach (char letter in Input.inputString)
         {
+            if (!IsAllowedCharacter(letter)) continue;
             WordManager.Instance.TypeLetter(letter);
-            // Debug.Log("Typed letter: " + letter);
+            Debug.Log("Typed letter: " + letter);
         }
     }
+
+    /// <summary>
+    /// Only alphabetic characters, space and - are allowed
+    /// </summary>
+    /// <param name="_letter"></param>
+    /// <returns></returns>
+    private bool IsAllowedCharacter(char _letter)
+    {
+        Debug.Log("Allowed character: " + _letter);
+        return char.IsLetter(_letter) || _letter == ' ' || _letter == '-';
+    }
+
 }
